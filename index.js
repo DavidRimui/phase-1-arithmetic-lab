34ccfe9da47b2a616c5c333039155a1e2a89717d
DavidRimui <110022594+DavidRimui@users.noreply.github.com>
2022-11-29 08:59:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-let num1 = 31;
+const num1 = 31;
+const num2 = 2;
+const num3 = 20;
 
-let num2 = 2;
+const num4 = 5;
 
-let num3 = 20;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 1;
+const mod  = num3 % num4;
 
-let num4 = 5;
-
-var multiply = num1 * num2;
-
-var random = Math.floor(Math.random() * 10) + 1;
-
-var mod  = num3 / num4;
-
-var max = Math.max(10, 15, 2, 20);
+const max = Math.max(10, 15, 2, 20);
